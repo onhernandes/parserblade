dfa9a68be1d8f07c57304f25c89c4ef571f6a3b3
Matheus Hernandes <midia.matheus@gmail.com>
2020-07-20 01:48:26 -0300

    Add csv support

--- src/strategies/index.js
+++ src/strategies/index.js
@@ -1,5 +1,6 @@
 const Json = require('./Json')
 const Xml = require('./Xml')
 const Yaml = require('./Yaml')
+const Csv = require('./Csv')
 
-module.exports = { Json, Xml, Yaml }
+module.exports = { Json, Xml, Yaml, Csv }
